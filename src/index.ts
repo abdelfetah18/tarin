d9bc8bf8b7be79e2215b9fea9f8fb0b15a02a421
abdelfetah18 <nstart0319@gmail.com>
2025-01-27 16:17:57 +0100

    expose Result class

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import Endpoint, { endpoint } from "./Endpoint";
 import Tarin from "./Tarin";
 import * as SchemaValidator from "./SchemaValidator";
 import OpenAPIInterpreter from "./OpenAPI/OpenAPIInterpreter";
+import Result from "./Result";
 
 export {
     Tarin,
     Endpoint,
     SchemaValidator,
     endpoint,
-    OpenAPIInterpreter
+    OpenAPIInterpreter,
+    Result,
 };
